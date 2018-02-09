e64c581f3ae8d8fdfd924ce6c6231b2b06538cb9
Quy Doan <quydoantran@gmail.com>
2018-02-09 10:19:54 -0500

    Add pg database

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180207114934) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "customers", force: :cascade do |t|
     t.string "name"
     t.string "location"
